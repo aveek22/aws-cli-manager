5b967f7bb6c2c46347a0eb40dc8ca66944c5337c
Aveek Das <aveek.das@dpgmedia.be>
2022-08-23 16:37:26 +0200

    Added dev

---
+++ shell_scripts/profile-1.sh
@@ -0,0 +1,4 @@
+source /Users/adas/codebase/personal/projects/aws-cli-manager/.venv/bin/activate
+
+/Users/adas/codebase/personal/projects/aws-cli-manager/.venv/bin/python /Users/adas/
+codebase/personal/projects/aws-cli-manager/app/app.py --profile profile-1
